4fa6ae052ed40fcab42454c12b28a07f88f02270
gennadybb <gennady.berezinsky@gmail.com>
2024-07-17 15:59:09 +0300

    add rule

---
+++ src/Rules/SetIdentityDataBeenCalledRule.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Rules;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+
+class SetIdentityDataBeenCalledRule implements Rule
+{
+
+    public function getNodeType(): string
+    {
+        return Node\Stmt\ClassMethod::class;
+    }
+
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (!$node->getType()) {
+            return [];
+        }
+
+        if (!in_array('App\Messenger\MessageHandler\AbstractAuditedMessageHandler', $scope->getClassReflection()->getParentClassesNames())) {
+            return [];
+        }
+
+        if ($node->name->name === '__invoke') {
+            $collectedMethodCalls = [];
+            foreach ($node->stmts as $value) {
+                if ($value instanceof Node\Stmt\Expression) {
+                    if($value->expr->name->name) {
+                        $collectedMethodCalls[] = $value->expr->name->name;
+                    }
+                }
+            }
+
+
+            if (!in_array('setAuditIdentityData', $collectedMethodCalls)) {
+                return ['Messages that extends AbstractAuditedMessageHandler should call setAuditIdentityData in __invoke'];
+            }
+        }
+
+        return [];
+    }
+}
