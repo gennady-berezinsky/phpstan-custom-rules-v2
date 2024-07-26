f191a99fb488861b320a0371836bb11369d38d29
gennadybb <gennady.berezinsky@gmail.com>
2024-07-26 12:06:39 +0300

    add new rule

---
+++ src/Rules/MandatoryCompanyFilterInterfaceImplementationRule.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Rules;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/**
+ * @implements Rule<Node\Stmt\Class_>
+ */
+class MandatoryCompanyFilterInterfaceImplementationRule implements Rule
+{
+    public function getNodeType(): string
+    {
+        return Node\Stmt\Class_::class;
+    }
+
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (!$node->getType()) {
+            return [];
+        }
+
+        $hasInterfase = false;
+        $declaredClassParts = $node->namespacedName->parts;
+        if (in_array('Entity', $declaredClassParts) && in_array('App', $declaredClassParts)) {
+            if ($node->getProperty('company')) {
+                foreach ($node->implements as $intrf) {
+                    if (str_contains($intrf->toString(), 'CompanyObjectInterface') || str_contains($intrf->toString(), 'CompanyUserInterface')) {
+                        $hasInterfase = true;
+                        break;
+                    }
+                }
+
+                if (!$hasInterfase) {
+                    return [
+                        RuleErrorBuilder::message('Entities with property $company should implement CompanyObjectInterface or CompanyUserInterface. Check this Entity and implement interface or add error to ignore.')
+                            ->identifier('CustomRule.MandatoryCompanyFilterInterfaceImplementationRule')
+                            ->build()
+                    ];
+                }
+            }
+        }
+
+        return [];
+    }
+
+}
